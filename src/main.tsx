ed57ecee24ba564aa27368509c2eea738524cf79
YoungLee <youngleepost@163.com>
2026-06-03 17:34:17 +0800

    fix: add NuqsAdapter provider to fix blank page after login

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from '@/components/ui/sonner'
+import { NuqsAdapter } from 'nuqs/adapters/react-router/v7'
 import { App } from './app'
 import '@/styles/globals.css'
 
@@ -18,10 +19,12 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <Toaster position="top-right" richColors />
-      </QueryClientProvider>
+      <NuqsAdapter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <Toaster position="top-right" richColors />
+        </QueryClientProvider>
+      </NuqsAdapter>
     </BrowserRouter>
   </StrictMode>
 )
